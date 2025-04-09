e79217fd09842f5b832e76ec3868503123e6e41d
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-04-09 19:44:45 +0530

    revert

--- build.sh
+++ build.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 set -e
 
-if [ "$(uname -s)" = "Darwin" ]; then
-	MOUNT_POINT="/Volumes/RISCVToolchain"
-	WORK_DIR="$MOUNT_POINT/workdir"
-
-	hdiutil create -size 8g -fs "Case-sensitive APFS" -volname RISCVToolchain -type SPARSE ~/RISCVToolchain.sparseimage
-	hdiutil attach ~/RISCVToolchain.sparseimage
-
-	mkdir -p "$WORK_DIR"
-	cd "$WORK_DIR"
-else
-    WORK_DIR="$(pwd)/workdir"
-    mkdir -p "$WORK_DIR"
-    cd "$WORK_DIR"
-fi
-
 HOST=$1
-PREFIX=$WORK_DIR/.dist/$HOST
+PREFIX=$PWD/.dist/$HOST
 
 # Extract bit size from host name
 if [[ $HOST =~ riscv32im ]]; then
   ARCH="rv32im"
   ABI="ilp32"
-elif [[ $HOST =~ riscv64ima ]]; then
-  ARCH="rv64ima"
+elif [[ $HOST =~ riscv64im ]]; then
+  ARCH="rv64im"
   ABI="lp64"
 else
   echo "Unknown host: $HOST"
@@ -40,20 +25,11 @@ fi
 # set up the gnu toolchain
 git clone https://github.com/riscv-collab/riscv-gnu-toolchain
 cd riscv-gnu-toolchain
-
-# Use $SED with appropriate syntax based on whether it's BSD sed on macOS
-if [ "$SED" = "sed" ] && [ "$(uname -s)" = "Darwin" ]; then
-    # BSD sed on macOS requires an empty string after -i
-    $SED -i '' '/shallow = true/d' .gitmodules
-    $SED -i '' 's/--depth 1//g' Makefile.in
-else
-    # GNU sed syntax (works for gsed on macOS or sed on Linux)
-    $SED -i '/shallow = true/d' .gitmodules
-    $SED -i 's/--depth 1//g' Makefile.in
-fi
+$SED -i '/shallow = true/d' .gitmodules
+$SED -i 's/--depth 1//g' Makefile.in
 
 echo "building toolchain for host: $HOST, arch: $ARCH, abi: $ABI"
-./configure --prefix=$PREFIX --with-cmodel=medany --disable-gdb --with-arch=$ARCH --with-abi=$ABI --with-languages=c,c++,go
+./configure --prefix=$PREFIX --with-cmodel=medany --disable-gdb --with-arch=$ARCH --with-abi=$ABI
 make -j$(nproc 2>/dev/null || sysctl -n hw.logicalcpu)
 
 cd ..
